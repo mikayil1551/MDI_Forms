df99c69c1906c449ada7c2b96c10d1340035b5cd
Sadigzade Mikayil <75858136+mikayil1551@users.noreply.github.com>
2021-04-25 02:46:28 +0300

    Add files via upload

---
+++ MDI_Forms/MDI_Forms/MusteriForm.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace MDI_Forms
+{
+    partial class MusteriForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // MusteriForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 132);
+            this.Name = "MusteriForm";
+            this.Text = "MusteriForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
